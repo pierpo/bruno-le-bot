6c597e05abb2274912ed0af756f287f5ff07a2d7
Pierre Poupin <pierrep@bam.tech>
2020-03-20 00:58:50 +0100

    add nlp

--- bruno.js
+++ bruno.js
@@ -1,6 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const { NlpManager } = require('node-nlp');
+
+const manager = new NlpManager({ languages: ['fr'] });
+// Adds the utterances and intents for the NLP
+manager.addDocument('fr', 'je peux te prendre une ambrée', 'serve.beer');
+manager.addDocument('fr', 'une petite ambrée', 'serve.beer');
+manager.addDocument('fr', 'je veux bien une ambrée', 'serve.beer');
+manager.addDocument('fr', 'une ambrée stp', 'serve.beer');
+manager.addDocument('fr', 'une ambrée', 'serve.beer');
+
+// Train also the NLG
+manager.addAnswer('fr', 'serve.beer', 'En voilà une !');
+manager.addAnswer('fr', 'serve.beer', 'Voilà pour toi !');
+manager.addAnswer('fr', 'serve.beer', 'Tiens, santé !');
 
 const normalizeString = str =>
   str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
@@ -10,27 +24,37 @@ const isBeingPinged = message => {
   return message.mentions.users.get(botId);
 };
 
-client.on('ready', () => {
-  console.log(
-    `Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`,
-  );
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
+(async () => {
+  await manager.train();
+  manager.save();
+  // const response = await manager.process('en', 'I should go now');
+  // console.log(response);
 
-client.on('message', async message => {
-  // This event will run on every single message received, from any channel or DM.
+  client.on('ready', () => {
+    console.log(
+      `Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`,
+    );
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  });
 
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
-  if (message.author.bot) return;
+  client.on('message', async message => {
+    // This event will run on every single message received, from any channel or DM.
 
-  if (!isBeingPinged(message)) return;
+    // It's good practice to ignore other bots. This also makes your bot ignore itself
+    // and not get into a spam loop (we call that "botception").
+    if (message.author.bot) return;
 
-  const messageWithoutMention = message.content.match(/^<@!\d+>(.*)$/)[1];
-  const parsedMessage = normalizeString(messageWithoutMention.toLowerCase());
+    if (!isBeingPinged(message)) return;
 
-  // Let's go with a few common example commands! Feel free to delete or change those.
-  console.log('parsedMessage: ', parsedMessage);
-});
+    const messageWithoutMention = message.content.match(/^<@!\d+>(.*)$/)[1];
+    const parsedMessage = normalizeString(messageWithoutMention.toLowerCase());
 
-client.login(config.token);
+    // Let's go with a few common example commands! Feel free to delete or change those.
+    // console.log('parsedMessage: ', parsedMessage);
+    const leMess = await manager.process('fr', parsedMessage);
+    console.log('leMess: ', leMess);
+    message.channel.send(leMess.answer);
+  });
+
+  client.login(config.token);
+})();
