c449bf7aa04032b1a87229020e9bbbe4687715d3
Pierre Poupin <pierrep@bam.tech>
2020-03-20 01:27:22 +0100

    add many messages

--- bruno.js
+++ bruno.js
@@ -4,17 +4,114 @@ const config = require('./config.json');
 const { NlpManager } = require('node-nlp');
 
 const manager = new NlpManager({ languages: ['fr'] });
-// Adds the utterances and intents for the NLP
-manager.addDocument('fr', 'je peux te prendre une ambrée', 'serve.beer');
-manager.addDocument('fr', 'une petite ambrée', 'serve.beer');
-manager.addDocument('fr', 'je veux bien une ambrée', 'serve.beer');
-manager.addDocument('fr', 'une ambrée stp', 'serve.beer');
-manager.addDocument('fr', 'une ambrée', 'serve.beer');
 
-// Train also the NLG
-manager.addAnswer('fr', 'serve.beer', 'En voilà une !');
-manager.addAnswer('fr', 'serve.beer', 'Voilà pour toi !');
-manager.addAnswer('fr', 'serve.beer', 'Tiens, santé !');
+manager.addNamedEntityText('cocktail', 'cdecale', ['fr'], ["C'Décalé"]);
+manager.addNamedEntityText('cocktail', 'mojito', ['fr'], ['mojito']);
+
+manager.addNamedEntityText('beer', 'ambree', ['fr'], ['ambrée']);
+manager.addNamedEntityText('beer', 'blonde', ['fr'], ['blonde']);
+manager.addNamedEntityText('beer', 'rousse', ['fr'], ['rousse']);
+manager.addNamedEntityText('beer', 'blanche', ['fr'], ['blanche']);
+manager.addNamedEntityText('beer', 'brune', ['fr'], ['brune']);
+manager.addNamedEntityText('beer', 'juda', ['fr'], ['Juda']);
+manager.addNamedEntityText('beer', 'pinte', ['fr'], ['pinte']);
+manager.addNamedEntityText('beer', 'ipa', ['fr'], ['ipa']);
+
+// demande de biere
+manager.addDocument('fr', 'tu me sers une %beer%', 'serve.beer');
+manager.addDocument('fr', 'je peux te prendre une %beer%', 'serve.beer');
+manager.addDocument('fr', 'une petite %beer%', 'serve.beer');
+manager.addDocument('fr', 'je veux bien une %beer%', 'serve.beer');
+manager.addDocument('fr', 'une %beer% stp', 'serve.beer');
+manager.addDocument('fr', 'une %beer%', 'serve.beer');
+
+manager.addAnswer('fr', 'serve.beer', 'En voilà une ! :beer:');
+manager.addAnswer('fr', 'serve.beer', 'En voilà une pour toi ! :beer:');
+manager.addAnswer('fr', 'serve.beer', 'Voilà pour toi ! :beer:');
+manager.addAnswer('fr', 'serve.beer', 'Tiens, santé ! :beer:');
+
+// demande de cocktail
+manager.addDocument('fr', 'tu me sers un %cocktail%', 'serve.cocktail');
+manager.addDocument('fr', 'je peux te prendre un %cocktail%', 'serve.cocktail');
+manager.addDocument('fr', 'un petit %cocktail%', 'serve.cocktail');
+manager.addDocument('fr', 'je veux bien un %cocktail%', 'serve.cocktail');
+manager.addDocument('fr', 'un %cocktail% stp', 'serve.cocktail');
+manager.addDocument('fr', 'un %cocktail%', 'serve.cocktail');
+
+manager.addAnswer('fr', 'serve.cocktail', 'En voilà un ! :cocktail:');
+manager.addAnswer('fr', 'serve.cocktail', 'En voilà un pour toi ! :cocktail:');
+manager.addAnswer('fr', 'serve.cocktail', "C'est parti ! :cocktail:");
+manager.addAnswer('fr', 'serve.cocktail', 'Voilà pour toi ! :cocktail:');
+manager.addAnswer('fr', 'serve.cocktail', 'Tiens, santé ! :cocktail:');
+
+// tu as quoi
+manager.addDocument('fr', 'tu as quoi ?', 'what.do.you.serve');
+manager.addDocument('fr', 'tu sers quoi ?', 'what.do.you.serve');
+manager.addDocument('fr', "qu'est-ce que t'as ?", 'what.do.you.serve');
+manager.addDocument('fr', "qu'est-ce que tu sers ?", 'what.do.you.serve');
+
+manager.addAnswer(
+  'fr',
+  'what.do.you.serve',
+  "Ce soir j'ai juda, blanche, blonde, IPA, mojito, C'Décalé...",
+);
+
+manager.addAnswer(
+  'fr',
+  'what.do.you.serve',
+  "Si tu veux je te sers une juda, blanche, blonde, IPA, mojito, C'Décalé...",
+);
+
+// salut
+manager.addDocument('fr', 'salut', 'greetings.hello');
+manager.addDocument('fr', 'hello', 'greetings.hello');
+manager.addDocument('fr', 'bonsoir', 'greetings.hello');
+manager.addDocument('fr', 'bonjour', 'greetings.hello');
+manager.addDocument('fr', 'yo', 'greetings.hello');
+
+manager.addAnswer('fr', 'greetings.hello', 'Salut ! :wave:');
+manager.addAnswer('fr', 'greetings.hello', 'Bien le bonsoir ! :wave:');
+manager.addAnswer('fr', 'greetings.hello', 'Salut ça va ? :wave:');
+manager.addAnswer(
+  'fr',
+  'greetings.hello',
+  'La forme ? Je te sers quoi ? :wave:',
+);
+
+// ca va
+manager.addDocument('fr', 'ça va ?', 'greetings.how.is.it.going');
+manager.addDocument('fr', 'ça roule ?', 'greetings.how.is.it.going');
+manager.addDocument('fr', 'tu vas bien ?', 'greetings.how.is.it.going');
+manager.addDocument('fr', 'comment tu vas ?', 'greetings.how.is.it.going');
+manager.addDocument('fr', 'comment tu vas ?', 'greetings.how.is.it.going');
+
+manager.addAnswer(
+  'fr',
+  'greetings.how.is.it.going',
+  "Ca roule et toi ? Qu'est-ce que je te sers ?",
+);
+manager.addAnswer(
+  'fr',
+  'greetings.how.is.it.going',
+  'Bien bien, les affaires reprennent ! Tu veux un truc à boire ?',
+);
+manager.addAnswer(
+  'fr',
+  'greetings.how.is.it.going',
+  'Au top et toi ? Tu veux quelque chose à boire ?',
+);
+
+// au revoir
+manager.addDocument('fr', 'ciao', 'greetings.bye');
+manager.addDocument('fr', 'au revoir', 'greetings.bye');
+manager.addDocument('fr', 'a bientot', 'greetings.bye');
+manager.addDocument('fr', 'a+', 'greetings.bye');
+manager.addDocument('fr', '++', 'greetings.bye');
+
+manager.addAnswer('fr', 'greetings.bye', 'A la prochaine ! :wave:');
+manager.addAnswer('fr', 'greetings.bye', 'A plus ! :wave:');
+manager.addAnswer('fr', 'greetings.bye', 'Bye ! :wave:');
+manager.addAnswer('fr', 'greetings.bye', 'Ciao ! :wave:');
 
 const normalizeString = str =>
   str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
@@ -26,6 +123,7 @@ const isBeingPinged = message => {
 
 (async () => {
   await manager.train();
+  console.log('Done training!');
   manager.save();
   // const response = await manager.process('en', 'I should go now');
   // console.log(response);
@@ -38,10 +136,7 @@ const isBeingPinged = message => {
   });
 
   client.on('message', async message => {
-    // This event will run on every single message received, from any channel or DM.
-
-    // It's good practice to ignore other bots. This also makes your bot ignore itself
-    // and not get into a spam loop (we call that "botception").
+    console.log('Received message!');
     if (message.author.bot) return;
 
     if (!isBeingPinged(message)) return;
@@ -49,11 +144,11 @@ const isBeingPinged = message => {
     const messageWithoutMention = message.content.match(/^<@!\d+>(.*)$/)[1];
     const parsedMessage = normalizeString(messageWithoutMention.toLowerCase());
 
-    // Let's go with a few common example commands! Feel free to delete or change those.
-    // console.log('parsedMessage: ', parsedMessage);
-    const leMess = await manager.process('fr', parsedMessage);
-    console.log('leMess: ', leMess);
-    message.channel.send(leMess.answer);
+    console.log('Processing...');
+    const nlpResult = await manager.process('fr', parsedMessage);
+    if (!nlpResult.answer) return;
+    console.log('Sending message!');
+    message.channel.send(nlpResult.answer);
   });
 
   client.login(config.token);
